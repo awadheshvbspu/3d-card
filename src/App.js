977ab67e613b89059234141e03dc92650a97d6da
Awadhesh Yadav <95018843+awadheshvbspu@users.noreply.github.com>
2023-08-20 02:44:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/Card'
+
+export default function App() {
+  return (
+    <>
+    <div className='w-full bg-[#b1a29d] h-screen flex items-center justify-center'>
+    <Card/>
+    </div>
+    </>
+  )
+}
+
